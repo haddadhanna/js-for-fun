587d54d34c0de0b7ccd60f282a14e18a6431cc30
Hanna Haddad <haddad.inbox@gmail.com>
2019-05-09 12:17:32 +0300

    #add drag

--- ball-animate-svg/js/ui.js
+++ ball-animate-svg/js/ui.js
@@ -4,18 +4,16 @@ APP.UI = function ()
     var _container;
     var _poolContainer;
     var _isGameOnUI = false;
-    var _ballMovement = {
-        IsHoldingTheBall: false,
-        StartX: 0,
-        StartY: 0
+    var _dragTheBall = {
+        IsDoingDrag: false,
+        Item: null
     };
-
 
     var _animate = function (ctx, speedX, speedY, direction)
     {
         return function ()
         {
-            if (_isGameOnUI === true && _ballMovement.IsHoldingTheBall === false)
+            if (_isGameOnUI === true)
             {
                 var radius = parseFloat(ctx.getAttributeNS(null, 'r'));
                 if (direction === APP.DIRECTION_HORIZONTAL || direction === APP.DIRECTION_DIAGONAL)
@@ -35,27 +33,46 @@ APP.UI = function ()
             }
         };
     };
-    var _lockTheBall = function (e)
+    var _startDrag = function (e)
     {
-        _ballMovement.IsHoldingTheBall = true;
-        _ballMovement.StartX = e.clientX;
-        _ballMovement.StartY = e.clientY;
+        if (e.target.tagName === 'circle')
+        {
+            _dragTheBall.Item = e.target;
+            _dragTheBall.IsDoingDrag = true;
+        }
     };
-    var _releaseTheBall = function ()
+    var _drag = function (e)
     {
-        _ballMovement.IsHoldingTheBall = false;
-        _ballMovement.StartX = 0;
-        _ballMovement.StartY = 0;
+        if (_dragTheBall.IsDoingDrag === true && _dragTheBall.Item !== null)
+        {
+            //Returns a DOMMatrix representing the matrix that transforms the current element's coordinate system
+            //to the coordinate system of the SVG viewport for the SVG document fragment.
+            var matrix = _container.getScreenCTM()
+            //inverse the matrix object
+                .inverse();
+            var point = _container.createSVGPoint();
+            point.x = e.clientX;
+            point.y = e.clientY;
+            point = point.matrixTransform(matrix);
+            _dragTheBall.Item.setAttributeNS(null, 'cx', point.x);
+            _dragTheBall.Item.setAttributeNS(null, 'cy', point.y);
+        }
     };
-    var _moveTheBall = function (e)
+    var _endDrag = function (e)
     {
-      //todo
+        _dragTheBall.IsDoingDrag = false;
+        _dragTheBall.Item = null;
     };
 
     this.init = function ()
     {
         _container = document.getElementById('svgPool');
         _poolContainer = document.getElementById('pool');
+
+        _container.addEventListener('mousedown', _startDrag);
+        _container.addEventListener('mousemove', _drag);
+        _container.addEventListener('mouseup', _endDrag);
+        _container.addEventListener('mouseleave', _endDrag);
     };
 
     this.updateStatus = function (status)
@@ -78,11 +95,8 @@ APP.UI = function ()
             circle.setAttributeNS(null, 'cy', theY);
             circle.setAttributeNS(null, 'r', theR);
             circle.setAttributeNS(null, 'style', 'fill:' + ball.color());
-            circle.addEventListener('mousedown', _lockTheBall);
-            circle.addEventListener('mouseup', _releaseTheBall);
-            circle.addEventListener('mousemove', _moveTheBall);
             _container.appendChild(circle);
-          //  setInterval(_animate(circle, ball.speed(), ball.speed(), ball.direction()), 10);
+           // setInterval(_animate(circle, ball.speed(), ball.speed(), ball.direction()), 10);
             this.feed('circle is added');
         }
     };
@@ -91,5 +105,4 @@ APP.UI = function ()
     {
         console.log(message);
     };
-
 };
